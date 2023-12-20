63fa72ff8816e23ecfad87c2dca3169831e2c2bf
Dhruvil Sudani <dhruvil.sudani10@gmail.com>
2023-12-20 09:36:21 +0530

    display stored data

--- initials/_header.php
+++ initials/_header.php
@@ -14,7 +14,7 @@
           <a class="nav-link" href="/pdo_crud/create.php">Create</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
+          <a class="nav-link" href="/pdo_crud/api.php">API</a>
         </li>
       </ul>
       <span class="navbar-text">
